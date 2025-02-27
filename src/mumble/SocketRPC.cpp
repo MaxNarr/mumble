21632a5d17a8b2169535aced162d9243a9303343
MaxNarr <narr.max@gmail.com>
2025-02-27 19:20:24 +0100

    test print

--- src/mumble/SocketRPC.cpp
+++ src/mumble/SocketRPC.cpp
@@ -328,6 +328,7 @@ SocketRPC::SocketRPC(const QString &basename, QObject *p) : QObject(p) {
 			pipepath = xdgRuntimeDir.absoluteFilePath(basename + QLatin1String("Socket"));
 		} else {
 			pipepath = QDir::home().absoluteFilePath(QLatin1String(".") + basename + QLatin1String("Socket"));
+			std::cout << "Socket Path: " << pipepath.toStdString() << std::endl;
 		}
 	}
 

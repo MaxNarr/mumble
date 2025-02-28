59e61ec28b7dcb51a0f3a526ea4658bbcb5dffc6
MaxNarr <narr.max@gmail.com>
2025-02-28 23:52:15 +0100

    try button press

--- pi/testEncoder.py
+++ pi/testEncoder.py
@@ -1,10 +1,26 @@
 from gpiozero import RotaryEncoder
+from gpiozero import Button
+from signal import pause
 
 encoder = RotaryEncoder(17, 18, wrap=True)  # Replace with your GPIO pins
 
 last_position = encoder.value
+def on_press():
+    print("Button pressed!")
+
+def on_release():
+    print("Button released!")
+
+button = Button(17)
+
+button.when_pressed = on_press  # Trigger on press
+button.when_released = on_release  # Trigger on release
 
 while True:
     if encoder.value != last_position:
         print(f"Rotary position: {encoder.value}")
         last_position = encoder.value
+
+
+
+

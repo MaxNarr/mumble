51a1edc76f7d4fa6443cf789108d9b0cec1762cb
MaxNarr <narr.max@gmail.com>
2025-03-01 00:19:54 +0100

    no bounce for encoder

--- pi/testEncoder.py
+++ pi/testEncoder.py
@@ -2,7 +2,7 @@ from gpiozero import RotaryEncoder
 from gpiozero import Button
 from time import time
 
-encoder = RotaryEncoder(18, 17, wrap=False,max_steps=0, bounce_time=0.01)  # Replace with your GPIO pins
+encoder = RotaryEncoder(18, 17, wrap=False,max_steps=0)  # Replace with your GPIO pins
 
 last_position = encoder.value
 last_press_time = 0

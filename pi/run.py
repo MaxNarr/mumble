f2e7ff1a3a4aeb2786846480e345dc02c3f808b1
MaxNarr <narr.max@gmail.com>
2025-03-03 22:58:37 +0100

    fixing scaling numbers for volume

--- pi/run.py
+++ pi/run.py
@@ -123,7 +123,7 @@ def monitor_encoder2(manager):
         new_value = encoder.value
         if new_value != old_value:
             print("encoder change")
-            manager.getTile().set_volume(math.floor(encoder.value * 10))
+            manager.getTile().set_volume(math.floor(encoder.value * 3))
  
             old_value = new_value
         time.sleep(0.05)  # or 0.005 or whatever

543eb34c6c679a09beaf5763f0e3f0c14c5a43a1
MaxNarr <narr.max@gmail.com>
2025-03-08 19:47:03 +0100

    added platform parameter for mumble startup

--- pi/mumbleRPC.py
+++ pi/mumbleRPC.py
@@ -2,7 +2,7 @@ import subprocess
 import jackcontroll
 from displayLib import Tile
 BASEDIR_MUMBLE = "/home/coms2/intercom_mumble/mumble/build/"  # Adjust to your environment
-
+PLATFORM_PARAM = "-platform offscreen"
 def talk(channel: Tile , on=True, toggle=False):
     """
     Example: mumble rpc shouttochannel_{channel_id}
@@ -13,11 +13,11 @@ def talk(channel: Tile , on=True, toggle=False):
         channel.talking = on
 
     if channel.talking:
-        cmd = f"{BASEDIR_MUMBLE}mumble rpc shouttochannel_{channel.id}"
+        cmd = f"{BASEDIR_MUMBLE}mumble rpc shouttochannel_{channel.id} {PLATFORM_PARAM}"
         output = run_command(cmd)
         jackcontroll.connectSideToneJack()
     else:
-        cmd = f"{BASEDIR_MUMBLE}mumble rpc stopshouttochannel_{channel.id}"
+        cmd = f"{BASEDIR_MUMBLE}mumble rpc stopshouttochannel_{channel.id} {PLATFORM_PARAM}"
         output = run_command(cmd)
         jackcontroll.disconnectSideToneJack()
     return output
@@ -26,7 +26,7 @@ def listen(channel: Tile ):
     """
     Example: mumble rpc listentochannelatvolume_{channel_id}_{volume}
     """
-    cmd = f"{BASEDIR_MUMBLE}mumble rpc listentochannelatvolume_{channel.id}_{channel.volume * 10}" #da mumble -30...30. wir senden aber nur bis 20
+    cmd = f"{BASEDIR_MUMBLE}mumble rpc listentochannelatvolume_{channel.id}_{channel.volume * 10} {PLATFORM_PARAM}" #da mumble -30...30. wir senden aber nur bis 20
     output = run_command(cmd)
     return output
 
@@ -35,7 +35,7 @@ def start_mumble():
     """
     Example: mumble
     """
-    cmd = f"{BASEDIR_MUMBLE}mumble"
+    cmd = f"{BASEDIR_MUMBLE}mumble {PLATFORM_PARAM}"
     output = run_command(cmd)
     return output
 
@@ -44,12 +44,12 @@ def get_channel_info():
     """
     Example: mumble rpc getchannelinfo
     """
-    cmd = f"{BASEDIR_MUMBLE}mumble rpc getchannelinfo"
+    cmd = f"{BASEDIR_MUMBLE}mumble rpc getchannelinfo {PLATFORM_PARAM}"
     output = run_command(cmd)
     return output
 
 def call(channel: Tile ):
-    cmd = f"{BASEDIR_MUMBLE}mumble rpc calltochannel_{channel.id}"
+    cmd = f"{BASEDIR_MUMBLE}mumble rpc calltochannel_{channel.id} {PLATFORM_PARAM}"
     output = run_command(cmd)
     return output
 
